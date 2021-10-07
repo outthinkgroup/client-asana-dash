53c8abb4419967532040a7e290c949e971029247
Joshua Kennedy <josh@outthinkgroup.com>
2021-10-07 15:56:35 -0400

    better ux for open closing calendar

--- src/TaskRangeCalendar.js
+++ src/TaskRangeCalendar.js
@@ -1,13 +1,31 @@
-import React, { useState } from "react";
+import React, { useLayoutEffect, useRef, useState } from "react";
 import styled from "styled-components";
 
 import Calendar, { CalendarWrapper } from "./Calendar.js";
 import CalendarIcon from "./CalendarIcon.js";
 
 export default function TaskDateRangeCalendar({ startOn, dueOn }) {
+  const ref = useRef();
   const [isOpen, setIsOpen] = useState(false);
+
+  useLayoutEffect(() => {
+    function watchClickInRef(e) {
+      const el = ref?.current;
+      if (!el || el.contains(e.target)) {
+        return;
+      }
+      setIsOpen((s) => s && false);
+    }
+    document.addEventListener("mousedown", watchClickInRef);
+    document.addEventListener("touchstart", watchClickInRef);
+    return () => {
+      document.removeEventListener("mousedown", watchClickInRef);
+      document.removeEventListener("touchstart", watchClickInRef);
+    };
+  }, [ref]);
+
   return (
-    <TaskDateRangeWrapper>
+    <TaskDateRangeWrapper ref={ref}>
       <button onClick={() => setIsOpen((s) => !s)}>
         <CalendarIcon />
       </button>
@@ -20,13 +38,17 @@ const TaskDateRangeWrapper = styled.div`
 	
 	${CalendarWrapper}{
     position: absolute;
-		z-index:99;
+		top:0;
+		z-index:9;
 		box-shadow: 0 50px 100px rgba(50,50,93,.1), 0 15px 35px rgba(50,50,93,.15), 0 5px 15px rgba(0,0,0,.1);
 		border-radius:6px;
 		overflow:hidden;
 	}
 
   button {
+		position:relative;
+		z-index:10;
+		border:1px solid #EEF3F8;
 		color:#1E3B8A;
     width: 2em;
     height: 2em;
@@ -34,10 +56,17 @@ const TaskDateRangeWrapper = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    border: none;
-    background: none;
+    background: #EEF3F8;
+		> * {
+			width:100%;
+			height:100%;
+			display:flex;
+			justify-content:center;
+			align-items:center;
+		}
     &:hover {
-      background: #EEF3F8;
+      background: #1E3A8A;
+			color:white;
     }
   }
   position: relative;
